3bdf281dc66eb1afd309283ce6a30b5126694b79
David Greenberg <dgrnbrg@David-Greenbergs-MacBook-Pro.local>
2011-08-18 22:56:09 -0400

    Initial Commit

---
+++ avr_comm_sys/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  avr_comm_sys
+//
+//  Created by David Greenberg on 8/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
